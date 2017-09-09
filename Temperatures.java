4af886dc75f0e8291a52825e266f4fce7d5bd0c3
jlpatneau <jlpatneau@gmail.com>
2017-09-09 14:40:54 -0500

    Temperatures assignment

---
+++ Temperatures.java
@@ -0,0 +1,50 @@
+
+public class Temperatures {
+ 
+    public static void main (String[] args) {
+        int tempGrid[][] = {{68, 70, 76, 70, 68, 71, 75}, {76, 76, 87, 84, 82, 75, 83}, {73, 72, 81, 78, 76, 73, 77}, {64, 65, 69, 68, 70, 74, 72}};
+        String days[] ={"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+        String times[] = {"7:00 AM", "3:00 PM", "7:00 PM", "3:00 AM"};
+        
+        float overallTotal = 0;
+        float avgPerDay[] = new float [7];
+        float avgPerTime[] = new float [4];
+
+        System.out.println("Temperature Chart");
+        for (int timeIndex=0; timeIndex<times.length; timeIndex++) {
+            for (int dayIndex=0; dayIndex<days.length; dayIndex++) {
+                System.out.print(tempGrid[timeIndex][dayIndex] + "  ");
+            }
+            System.out.print("\n");
+        }
+        
+        //average temp for each day
+        System.out.println("\nAverage Temperature for each day of the week: ");
+        for (int dayIndex=0; dayIndex<days.length; dayIndex++) {
+            float dayTotal = 0;
+            for (int timeIndex=0; timeIndex<times.length; timeIndex++) {
+                dayTotal= dayTotal + tempGrid[timeIndex][dayIndex];
+            }
+            avgPerDay[dayIndex] = dayTotal / times.length;
+            System.out.println(days[dayIndex] + ": " + avgPerDay[dayIndex]);
+        }
+        
+        //average temp for each time
+        System.out.println("\nAverage Temperature for each time: ");
+        for (int timeIndex=0; timeIndex<times.length; timeIndex++) {
+            float timeTotal = 0;
+            for (int dayIndex=0; dayIndex<days.length; dayIndex++) {
+                timeTotal = timeTotal + tempGrid[timeIndex][dayIndex];
+                //System.out.print(timeTotal + " ");
+            }
+            //System.out.print("\n");
+            avgPerTime[timeIndex] = timeTotal / days.length;
+            System.out.println(times[timeIndex] + ": " + avgPerTime[timeIndex]);
+            overallTotal = avgPerTime[timeIndex] + overallTotal;
+        }
+                
+        //overall average temp
+        float overallAvg = overallTotal / times.length;
+        System.out.println("\nThe overall average temperature is : " + overallAvg);
+    }
+}
